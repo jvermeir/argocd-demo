d179e99987e7d85026d84d954c6ac3620372a913
Jan Vermeir <jvermeir@hotmail.com>
2020-04-05 19:19:16 +0200

    use explicit version

--- test-container/build.sh
+++ test-container/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [ $# -lt 1 ]; then
+    echo "Usage: ./build.sh <version>"
+    exit -1
+fi
+echo $1
+VERSION=$1
+
 git rev-parse --short HEAD > git-hash.txt
-docker build . -t jvermeir/date-time
-docker push jvermeir/date-time:latest
+docker build . -t jvermeir/date-time:$VERSION
+docker push jvermeir/date-time
